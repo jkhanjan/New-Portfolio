14d4ccccadf68484aada08d8bb16d185e0348e58
jkhanjan <jhakhanjan62@gmail.com>
2024-10-19 17:46:31 +0530

    still trying

--- src/components/Page1.jsx
+++ src/components/Page1.jsx
@@ -1,41 +1,6 @@
-import React, { lazy, Suspense, useEffect, useRef } from "react";
+import React from "react";
+import Scene2 from "../3Dscene/Scene2";
 import Navbar from "./Navbar";
-
-const Scene2 = lazy(() => import("../3Dscene/Scene2"));
-
-const OptimizedTitle = React.memo(() => {
-  const titleRef = useRef(null);
-
-  useEffect(() => {
-    const el = titleRef.current;
-    if (el) {
-      setTimeout(() => {
-        el.style.opacity = "0.75";
-        el.setAttribute("data-scroll", "");
-        el.setAttribute("data-scroll-speed", "0.1");
-      }, 100);
-    }
-  }, []);
-
-  return (
-    <h1
-      ref={titleRef}
-      className="text-[5rem] sm:text-[20rem] opacity-0 sm:tracking-[0px] leading-none"
-    >
-      FRONTEND DEVELOPER & FREELANCER
-    </h1>
-  );
-});
-
-const BackgroundCircle = React.memo(() => (
-  <div
-    className="flex w-full justify-end absolute z-[100] bottom-0"
-    data-scroll
-    data-scroll-speed="0.5"
-  >
-    <div className="w-[250px] h-[250px] sm:h-[400px] sm:w-[400px] bottom-[-17vh] right-[-40%] sm:right-[-14%] absolute bg-gray-950 opacity-80 rounded-full"></div>
-  </div>
-));
 
 const Page1 = () => {
   return (
@@ -44,20 +9,31 @@ const Page1 = () => {
       className="w-full h-screen flex flex-col justify-between items-center p-4 relative sm:px-10 sm:py-6 overflow-hidden z-[2]"
       data-scroll-section
     >
-      <nav className="flex w-full justify-between">
-        <h1 className="text-5xl text-black opacity-70">KHANJAN</h1>
+      <nav className="flex w-full justify-between ">
+        <div>
+          <h1 className="text-5xl text-black opacity-70">KHANJAN</h1>
+        </div>
         <Navbar />
       </nav>
 
-      <OptimizedTitle />
+      <h1
+        className=" text-[12vh] tracking-tight text-black opacity-75 leading-none sm:text-[39vh] sm:tracking-[0px] sm:leading-1"
+        data-scroll
+        data-scroll-speed="0.1"
+      >
+        FRONTEND DEVELOPER & FREELANCER
+      </h1>
 
-      <BackgroundCircle />
-
-      <Suspense fallback={<div>Loading 3D scene...</div>}>
-        <Scene2 />
-      </Suspense>
+      <div
+        className="flex w-full justify-end absolute z-[100] bottom-0"
+        data-scroll
+        data-scroll-speed="0.5"
+      >
+        <div className="w-[250px] h-[250px] sm:h-[400px] sm:w-[400px] bottom-[-17vh] right-[-40%] sm:right-[-14%] absolute bg-gray-950 opacity-80 rounded-full z-[10000]"></div>
+      </div>
+      <Scene2 />
     </div>
   );
 };
 
-export default React.memo(Page1);
+export default Page1;
